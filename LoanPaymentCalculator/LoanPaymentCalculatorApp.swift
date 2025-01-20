b13f8a513f9a2b0ecbad51756f216b80e887d44a
Kevin Wu <KWu@lcs.on.ca>
2025-01-20 09:52:58 -0500

    Initial Commit

---
+++ LoanPaymentCalculator/LoanPaymentCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanPaymentCalculatorApp.swift
+//  LoanPaymentCalculator
+//
+//  Created by Yixuan Wu on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct LoanPaymentCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
