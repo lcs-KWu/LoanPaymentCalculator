339d19c2057dfccf9b5039f7a89464d459772288
Kevin Wu <KWu@lcs.on.ca>
2025-01-21 12:20:34 -0500

    finished setting up guard for all the textfield

--- LoanPaymentCalculator/CalculatorView.swift
+++ LoanPaymentCalculator/CalculatorView.swift
@@ -17,24 +17,45 @@ struct CalculatorView: View {
     @State var years = ""
     @State var loanAmount = ""
     
+    
     //MARK: computed properties
-   
+    var result : String {
+        
+        return loanAmount * loanRatio * years / 12
+    }
     var  body: some View {
         NavigationStack{
             VStack (spacing:30){
-              Text("This is a Loan payment Calulator")
+                Text("This is a Loan payment Calulator")
                 Text("Enter values to see what you have to pay per month!")
                 TextField("Loan amount", text: $loanAmount)
                 TextField("Loan time", text: $years)
                 TextField("Loan ratio", text: $loanRatio)
-                
+                Text("\(result)")
             }
-        
-     
-        .navigationTitle("Loan Payment Calculator")
+            
+            
+            .navigationTitle("Loan Payment Calculator")
         }
     }
+    func CheckInput(){
+        guard let loanRatioInt = Int(loanRatio) else {
+            feedback = "Please provide an integer."
+            return
+        }
+        guard let loanAmountInt = Int(loanAmount)  else {
+            feedback = "Please provide an integer."
+            return
+        }
+        guard let loanYearInt = Int(years)  else {
+            feedback = "Please provide an integer."
+            return
+        }
+    }
+   
 }
+
+
 
 #Preview {
     CalculatorView()
