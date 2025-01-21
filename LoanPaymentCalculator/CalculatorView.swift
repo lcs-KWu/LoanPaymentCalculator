90a02ca0449b74beea81ec3b8b4103a585a7febf
Kevin Wu <KWu@lcs.on.ca>
2025-01-21 11:50:22 -0500

    finished set up stored propertie

--- LoanPaymentCalculator/CalculatorView.swift
+++ LoanPaymentCalculator/CalculatorView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct CalculatorView: View {
-    var body: some View {
+    
+    //MARK: stored properties
+    //feedback to user
+    @State var givenInput  = ""
+    @State var feedback  = ""
+    //must need condition
+    @State var loanRatio = Double.self
+    @State var years = Double.self
+    @State var loanAmount = Double.self
+    
+    //MARK: computed properties
+   
+    var  body: some View {
         NavigationStack{
-            VStack {
+            VStack (spacing:30){
               Text("This is a Loan payment Calulator")
                 Text("Enter values to see what you have to pay per month!")
+                TextField("Loan amount", text: $givenInput)
+                
             }
-        }
+        
         .padding()
         .navigationTitle("Loan Payment Calculator")
+        }
     }
 }
 
